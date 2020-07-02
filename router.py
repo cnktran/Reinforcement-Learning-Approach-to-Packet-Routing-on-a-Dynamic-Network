f3b653acbaa830a4fe488d37e7dc399c834913c4
hltung <51760305+hltung@users.noreply.github.com>
2020-07-02 15:14:02 -0700

    Update router.py

--- router.py
+++ router.py
@@ -8,38 +8,42 @@ Created on Wed Jul  1 16:46:11 2020
 import networkx as nx
 import matplotlib.pyplot as plt
 
-#router using dijkstra
-def dijkstra_router(network, pkt):
-    curr = pkt.get_curPos()
-    dest = pkt.get_endPos()
-    next_step = nx.dijkstra_path(network, curr, dest)[1] 
-    if is_capacity(network, next_step):
-        return curr
-    else:
-        send_packet(network, pkt, next_step)
-        return next_step
-        
-#router using floyd-warshall
-def fw_router(network, pkt):
-    curr = pkt.get_curPos()
-    dest = pkt.get_endPos()
-    preds, _ = nx.floyd_warshall_predecessor_and_distance(network)
-    next_step = nx.reconstruct_path(curr, dest, preds)[1]
-    if is_capacity(network, next_step):
-        return curr
-    else:
-        send_packet(network, pkt, next_step)
-        return next_step
+class Router:
+    def __init__(self):
+        pass
     
-#check if the node is at capacity
-def is_capacity(g, node):
-    return len(g.nodes[node]['current_queue']) == g.nodes[node]['max_queue']
-
-#send the packet the new node and update the current_queues
-def send_packet(g, pkt, next_step):
-    curr = pkt.get_curPos()
-    g.nodes[curr]['current_queue'].pop(0)
-    g.nodes[next_step]['current_queue'].append(pkt)
+    #router using dijkstra
+    def dijkstra_router(self, network, pkt):
+        curr = pkt.get_curPos()
+        dest = pkt.get_endPos()
+        next_step = nx.dijkstra_path(network, curr, dest)[1] 
+        if self.is_capacity(network, next_step):
+            return curr
+        else:
+            self.send_packet(network, pkt, next_step)
+            return next_step
+            
+    #router using floyd-warshall
+    def fw_router(self, network, pkt):
+        curr = pkt.get_curPos()
+        dest = pkt.get_endPos()
+        preds, _ = nx.floyd_warshall_predecessor_and_distance(network)
+        next_step = nx.reconstruct_path(curr, dest, preds)[1]
+        if self.is_capacity(network, next_step):
+            return curr
+        else:
+            self.send_packet(network, pkt, next_step)
+            return next_step
+        
+    #check if the node is at capacity
+    def is_capacity(g, node):
+        return len(g.nodes[node]['current_queue']) == g.nodes[node]['max_queue']
+    
+    #send the packet the new node and update the current_queues
+    def send_packet(g, pkt, next_step):
+        curr = pkt.get_curPos()
+        g.nodes[curr]['current_queue'].pop(0)
+        g.nodes[next_step]['current_queue'].append(pkt)
 
 # #test router functions
 # sample_g = nx.cycle_graph(5)
