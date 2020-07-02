e8b0476f5efa9cf9ca29890bb9cb1bc0d24b8e19
hltung <51760305+hltung@users.noreply.github.com>
2020-07-02 14:09:09 -0700

    Update router.py

--- router.py
+++ router.py
@@ -10,14 +10,29 @@ import matplotlib.pyplot as plt
 
 #router using dijkstra
 def dijkstra_router(network, curr, dest):
-    path = nx.dijkstra_path(network, curr, dest) 
-    return path[1]
-
+    next_step = nx.dijkstra_path(network, curr, dest)[1] 
+    if is_capacity(network, next_step):
+        return curr
+    else:
+        update(network, curr, next_step)
+        return next_step
+        
 #router using floyd-warshall
 def fw_router(network, curr, dest):
     preds, _ = nx.floyd_warshall_predecessor_and_distance(network)
-    path = nx.reconstruct_path(curr, dest, preds)
-    return path[1]
+    next_step = nx.reconstruct_path(curr, dest, preds)[1]
+    if is_capacity(network, next_step):
+        return curr
+    else:
+        update(network, curr, next_step)
+        return next_step
+    
+def is_capacity(g, node):
+    return g.nodes[node]['current_queue'] == g.nodes[node]['max_queue']
+
+def update(g, curr, next_step):
+    g.nodes[curr]['current_queue'] = g.nodes[curr]['current_queue'] - 1
+    g.nodes[next_step]['current_queue'] = g.nodes[next_step]['current_queue'] + 1
 
 # #test router functions
 # sample_g = nx.cycle_graph(5)
