ebbf07d611149e65154c9d0b536de05b403d4ab0
unknown <huanxiliu99@gmail.com>
2020-07-06 15:51:53 -0700

    Update dynetwork.py

--- dynetwork.py
+++ dynetwork.py
@@ -1,45 +1,51 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-import time
-import random
-import numpy as np
-import Packet
+from itertools import combinations
+from random import sample
+from queue import Queue
+import router as r
 import Packets
-
-# Question: Can I directly use functions in packet if I import Packets which included Packet
-'''PERSON class import WALLET， WALLET import MONEY. 
-In PERSON class directly use MONEY function '''
+import Packet
+import numpy as np
+import random
+import time
+import matplotlib.pyplot as plt
+import networkx as nx
+\documentclass[12pt]{article}
+\usepackage[utf8]{inputenc}
+\begin{document}
+\end{document}
 
 
 class DynamicNetwork(object):
-    def __init__(self, network, packets, rejections=0):
+    def __init__(self, network, packets=None, rejections=0, deliveries=0, qtable=None):
+
         self._network = network
         self._packets = packets
         self._rejections = rejections
+        self._deliveries = deliveries
+        self._qtable = qtable
 
     # Method declaration
     def randomGeneratePackets(self, num_packets_to_generate):
         tempList = []
-        for _ in range(num_packets_to_generate):
-            # random nodes initialization
-            startNode = self._network.nodes(
-                random.randint(0, len(self._network)))
-            endNode = self._network.nodes(
-                random.randint(0, len(self._network)))
-
+        for index in range(num_packets_to_generate):
+            num_nodes = len(list(self._network.nodes()))
+            endPoints = sample(list(combinations(range(0, num_nodes), 2)), 1)
+            startNode = endPoints[0][0]
+            endNode = endPoints[0][1]
             # if the node is full then assign to another
-            while(startNode['p_queue'].length() > startNode['max_capacity']):
-                startNode = self._network.nodes(
-                    random.randint(0, len(self._network)))
+            while (len(self._network.nodes[startNode]['p_queue'].queue) >= self._network.nodes[startNode]['max_capacity']):
+                endPoints = sample(
+                    list(combinations(range(0, num_nodes), 2)), 1)
+                startNode = endPoints[0][0]
+                endNode = endPoints[0][1]
 
-            # distinct starting and ending node
-            while (startNode == endNode):
-                endNode = self._network.nodes(
-                    random.randint(0, len(self._network)))
+            startNode = self._network.nodes[startNode]
+            endNode = self._network.nodes[endNode]
+            # creat packet object, assign a 0 weight
+            curPack = Packet.Packet(startNode, endNode, startNode, index, 0)
 
-            # creat packet object
-            # assign a 0 weight
-            curPack = Packet.Packet(startNode, endNode, startNode, 0)
+            # put curPack into startNode's queue
+            startNode['p_queue'].queue.append(curPack.get_index())
 
             tempList.append(curPack)
 
@@ -52,64 +58,50 @@ class DynamicNetwork(object):
 
 # -----------Below can be in the main funciton----------------------
 node_count = 100
-# Note that this is a measure of edges per added node, not total number of edges
+# # Note that this is a measure of edges per added node, not total number of edges
 edge_count = 5
 max_capacity = 5
-time_steps = 10
+time_steps = 2
 edge_removal_min = 0
 edge_removal_max = 10
 init_num_packets = 100
 
 # Initialize a Network using Networkx
 # Add attributes "capacity" and "queue" to keep track of packets
-G = nx.barabasi_albert_graph(node_count, edge_count)
-nx.set_node_attributes(G, max_capacity, 'max_capacity')
-
-#Used List instead of array for packet_queue#
-#https://www.geeksforgeeks.org/queue-in-python/#
-p_queue = [-1]*max_capacity
-nx.set_node_attributes(G, p_queue, 'p_queue')
-#nx.set_node_attributes(G, np.repeat(-1, max_capacity), 'p_queue')
+networkxGraph = nx.barabasi_albert_graph(node_count, edge_count)
+p_queue = Queue(max_capacity)
+nx.set_node_attributes(networkxGraph, max_capacity, 'max_capacity')
+nx.set_node_attributes(networkxGraph, p_queue, 'p_queue')
 
 # store in DynamicNetwork class
-initPackets = None
-G = DynamicNetwork(G, initPackets)
-
-# random generate packets for network
+G = DynamicNetwork(networkxGraph)
 G.randomGeneratePackets(init_num_packets)
-
-'''
-store packet index from Packets list
-for i in range(init_num_packets):
-    # temp is array: -1 * max_capacity
-    temp = np.repeat(-1, G._network.nodes[G._packets[i].startPos]['max capacity'])
-    #Question: pos is a boolean? Kyle 7/5/20
-    pos = (G._network.nodes[G._packets[i].startPos]['p_queue'] == temp)
-    G._network.nodes[G._packets[i].startPos]['p_queue'][pos][0] = i
-
-del temp
-del pos
-'''
-
 # Dynamic Edge Change
-r = Router()
-fixed_positions = nx.spring_layout(G)
+fixed_positions = nx.spring_layout(G._network)
 stripped_list = []
 for i in range(1, time_steps):
     # delete some edges
-    edges = G.edges()
+    edges = G._network.edges()
     deletion_number = random.randint(
         edge_removal_min, min(edge_removal_max, len(edges) - 1))
     strip = random.sample(edges, k=deletion_number)
-    G.remove_edges_from(strip)
+    G._network.remove_edges_from(strip)
     stripped_list.extend(strip)
     # restore some deleted edges
     restore_number = random.randint(0, len(stripped_list) - 1)
     restore = random.choices(stripped_list, k=restore_number)
-    G.add_edges_from(restore)
-    nx.draw(G, pos=fixed_positions, with_labels=True, font_weight='bold')
+    G._network.add_edges_from(restore)
+    # perform routing
+    r.dijkstra_router(G)
+    # draw network
+    node_queues = nx.get_node_attributes(G._network, 'p_queue')
+    node_labels = {}
+    for node, atr in node_queues.items():
+        node_labels[node] = atr.qsize()
+    nx.draw(G._network, pos=fixed_positions,
+            labels=node_labels, font_weight='bold')
     plt.savefig("network_images/dynet" + str(i) + ".png")
     plt.clf()
-
-    for pkt in G._packets:
-        r.dijkstra_router(G._network, pkt)
+    if G._deliveries == init_num_packets:
+        print("Simulation done in " + str(i))
+        break
